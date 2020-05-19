5a0a82d5fbfa525eab0494ff3264d326dd59bb79
alberto <alberto.websci@gmail.com>
2020-05-19 20:01:20 +0200

    first commit

---
+++ front/src/paint.js
@@ -0,0 +1,26 @@
+import {indexValues} from "./indexValues.mjs";
+
+const data = {
+    labels: Array.from({ length: 500 }, (_, i) => i),
+    datasets: [
+        {
+            name: "index value", type: "line",
+            values: indexValues
+        }
+    ]
+}
+
+const loadButton = document.getElementById('loadData')
+
+loadButton.addEventListener('click', e=> {
+    const chart = new frappe.Chart("#chart", {  // or a DOM element,
+        // new Chart() in case of ES6 module with above usage
+        title: "Xtal index value",
+        data: data,
+        type: 'line',
+        height: 250,
+        colors: ['#7cd6fd']
+    })
+
+})
+
